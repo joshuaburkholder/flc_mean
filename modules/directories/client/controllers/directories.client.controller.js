15254fc918997ea7fc5612b162d206a1bee31acf
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-02-17 14:28:36 -0500

    updated query filter

--- modules/directories/client/controllers/directories.client.controller.js
+++ modules/directories/client/controllers/directories.client.controller.js
@@ -6,9 +6,9 @@
     .module('directories')
     .controller('DirectoriesController', DirectoriesController);
 
-  DirectoriesController.$inject = ['$scope', '$state', '$window', 'Authentication', 'directoryResolve', 'Users'];
+  DirectoriesController.$inject = ['$scope', '$state', '$window', 'Authentication', 'directoryResolve', 'Users', '$filter'];
 
-  function DirectoriesController ($scope, $state, $window, Authentication, directory, Users) {
+  function DirectoriesController ($scope, $state, $window, Authentication, directory, Users, $filter) {
     var vm = this;
 
     vm.userList = Users.query();
@@ -19,12 +19,38 @@
     vm.remove = remove;
     vm.save = save;
 
+  //   $scope.userList = [
+  //   { displayName: displayName, email: email, username: username },
+  //   { displayName: displayName, email: email, username: username },
+  // ];
+
     // Remove existing Directory
     function remove() {
       if ($window.confirm('Are you sure you want to delete?')) {
         vm.directory.$remove($state.go('directories.list'));
       }
     }
+
+  
+// Filter Query results
+
+//     $scope.showdetails = function(getByID){
+         
+//          var found = $filter('userList')($scope.user, user_id);
+//          console.log(found);
+//          $scope.selected = JSON.stringify(found);
+//      }
+// };
+
+//     $scope.user = [
+//     {
+//       displayName: this.displayName, 
+//       username: this.username
+//     }, {
+//       displayName: this.displayName, 
+//       username: this.username
+//     }]
+
 
     // Save Directory
     function save(isValid) {
