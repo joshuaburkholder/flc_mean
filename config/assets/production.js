a9f7ed8e5be29709efb0d1ff6f1b13f33717f1a9
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-02-19 19:15:24 -0500

    updated default css settings

--- config/assets/production.js
+++ config/assets/production.js
@@ -4,8 +4,8 @@ module.exports = {
   client: {
     lib: {
       css: [
-        'public/lib/bootstrap/dist/css/bootstrap.min.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'modules/core/client/css/cerulean/theme/bootstrap.min.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css'
       ],
       js: [
         'public/lib/angular/angular.min.js',
